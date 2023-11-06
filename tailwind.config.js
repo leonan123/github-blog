84128479fdd9346b8ef1471bb718fde5ee63d648
leonan123 <leonanorth3@gmail.com>
2023-11-05 23:15:37 -0300

    chore: configure assets and extended tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          100: '#3294F8',
+        },
+
+        gray: {
+          100: '#E7EDF4',
+          200: '#C4D4E3',
+          300: '#AFC2D4',
+        },
+
+        cyan: {
+          100: '#7B96B2',
+          200: '#3A536B',
+          500: '#1C2F41',
+          600: '#112131',
+          700: '#0B1B2B',
+          800: '#071422',
+          900: '#040F1A',
+        },
+      },
+
+      fontFamily: {
+        primary: 'Nunito, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
-
